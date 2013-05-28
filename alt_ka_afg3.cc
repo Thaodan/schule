1758739821e173c26e156374be5a89422cfca9a7
Björn Bidar <theodorstormgrade@gmail.com>
2013-05-28 10:39:27 +0200

    added comments

--- alt_ka_afg3.cc
+++ alt_ka_afg3.cc
@@ -5,11 +5,13 @@ int main(void)
 {
   double erg=0, z[10];
   cout << endl << "Eingabe der 10 Werte" << endl;
+  //Ergänzung1
   for (int i=0; i < 10; i++)
     {
       cin << z[i];
     }
   
+  //Ergänzung2
   erg=z[0];
   for (int i=1; i < 10;i++)
     {
