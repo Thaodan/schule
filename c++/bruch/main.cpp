e7065daec130b4d3fec8742eb0c6036fbf8cc4cd
Björn Bidar <theodorstormgrade@gmail.com>
2014-02-20 11:00:23 +0100

    added

---
+++ c++/bruch/main.cpp
@@ -0,0 +1,11 @@
+#include "bruch.hpp"
+#include <iostream>
+
+int main(void)
+{
+    bruch b1(1,1);
+    bruch b2(1,1);
+    
+    b1.addieren(b2);
+    return 0;
+}
---
+++ c++/bruch/meth.cpp
@@ -0,0 +1,34 @@
+#include "bruch.hpp"
+#include <iostream>
+void bruch::print(void)
+{
+    std::cout << zahler << "/" < nenner << std::endl;
+}
+int  bruch::zahler(void)
+{
+    return v_zahler;
+}
+int bruch::nenner(void)
+{
+    return v_nenner;
+}
+void bruch::addieren(bruch ziel)
+{
+    if ( v_nenner + ziel.nenner() )
+    {
+	v_zahler = v_zahler + ziel.zahler();
+	v_nenner = v_nenner + ziel.nenner();
+    }
+}
+
+bruch::bruch()
+{
+    v_zahler = 0;
+    v_nenner = 1;
+}
+
+bruch::bruch(int zahler, int nenner)
+{
+    v_zahler = zahler;
+    v_nenner = nenner;
+}
